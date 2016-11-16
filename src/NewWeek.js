338398fd97e492383d303cc76b47aebcae40e26d
Justine Wang <justinelhwang@gmail.com>
2016-11-16 13:50:31 -0800

    Rewrote code for rendering the labels, summary, and resources for each week with a consolidated state data structure. Now need to rewrite the form and lifecycle.

---
+++ src/NewWeek.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class NewWeek extends Component {
+
+
+	render() {
+		return (
+			
+		);
+	}
+}
--- src/Week.js
+++ src/Week.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Week extends Component {
 	constructor(props) {
 		super(props);
-		debugger
+		
 	}
 
 
@@ -17,11 +17,17 @@ export default class Week extends Component {
 		// Map over the week object (we should have just one here)
 		// to render each resource list.
 
+		const resources = this.props.weekdata.resources.map((item, idx) => {
+			return <li key={idx}><a href={item.link}>{item.title}</a></li>
+		});
+
+		
+
 		return (
 			<div>
-				<p>{label}</p>
+				<h2>{label}</h2>
 				<p>{summary}</p>
-
+				<ul>{resources}</ul>
 			</div>
 		);
 	}
